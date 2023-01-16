0c270704165ff6c3b2a35f4b57dfbb5a7cf29042
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-01-16 12:53:07 +0100

    generate meme

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Data from "../memedata";
 
-export default function Main(){
-    return (
-        <main className="main">
-            <div>
-                <input type="text" placeholder="top text"/>
-                <input type="text" placeholder="buttom text"/>
-                <button>Get a new meme image 🖼️</button>
-            </div>
-        </main>
-    )
+export default function Main() {
+  let url;
+  const [genMeme, setGenMeme] = useState("https://i.imgflip.com/1g8my4.jpg");
+  function handleClick() {
+    const randomNumber = Math.floor(Math.random() * Data.data.memes.length);
+    url = Data.data.memes[randomNumber].url;
+    setGenMeme(url);
+  }
+
+  return (
+    <main className="main">
+      <div>
+        <input type="text" placeholder="top text" />
+        <input type="text" placeholder="buttom text" />
+        <button onClick={handleClick}>Get a new meme image 🖼️</button>
+      </div>
+      <img src={genMeme} className="memepic" />
+    </main>
+  );
 }
