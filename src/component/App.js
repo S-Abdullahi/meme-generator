3c9f28ba845c340b4dd6358f7c624895c6793c6b
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-01-18 11:58:26 +0100

    add text to meme

--- src/component/App.js
+++ src/component/App.js
@@ -1,22 +1,33 @@
-import React, {useState} from "react";
-import Nav from './Nav'
-import Main from './Main'
-import Data from '../memedata'
+import React, { useState } from "react";
+import Nav from "./Nav";
+import Main from "./Main";
+import Data from "../memedata";
 
 export default function App() {
+  let url;
+  const [meme, setMeme] = useState("https://i.imgflip.com/1ur9b0.jpg");
 
-    let url
-    const [meme, setMeme] = useState('https://i.imgflip.com/1ur9b0.jpg')
-    function getMeme(){
-        const memeRan = Math.floor(Math.random()*Data.data.memes.length)
-        url = Data.data.memes[memeRan].url
-        setMeme(url)
-    }
+  const [formData, setFormData] = useState({toptext: "", bottomtext: ""})
+
+  function handleChange(event){
+    const {name, value} = event.target
+    setFormData(prevData => {
+        return {
+            ...prevData, [name] : value
+        }
+    })
+  }
+
+  function getMeme() {
+    const memeRan = Math.floor(Math.random() * Data.data.memes.length);
+    url = Data.data.memes[memeRan].url;
+    setMeme(url);
+  }
 
   return (
     <>
-      <Nav/>
-      <Main memeImg={meme} handleClick={getMeme}/>
+      <Nav />
+      <Main memeImg={meme} handleClick={getMeme} formData={formData} handleChange={handleChange}/>
     </>
   );
 }
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 
 export default function Main(props) {
-    console.log(props)
+    
+console.log(props)
   return (
     <main className="main">
       <div>
-        <input type="text" placeholder="top text" />
-        <input type="text" placeholder="buttom text" />
+        <input type="text" placeholder="top text" name="toptext" value={props.formData.toptext} onChange={props.handleChange}/>
+        <input type="text" placeholder="buttom text" name="bottomtext" value={props.formData.bottomtext} onChange={props.handleChange}/>
         <button onClick={props.handleClick}>Get a new meme image 🖼️</button>
       </div>
       <div className="container">
         <img src={props.memeImg} className="memepic" />
-        <p className="text top">top text</p>
-        <p className="text bottom">bottom text</p>
+        <p className="text top">{props.formData.toptext}</p>
+        <p className="text bottom">{props.formData.bottomtext}</p>
       </div>
     </main>
   );
